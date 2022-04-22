e1253435266b1450ed62eeaf1f48028ca92a1789
HeyItsM3 <aldoyedro181@gmail.com>
2022-04-22 16:20:26 -0300

    Landing development

--- client/src/components/Pages/Landing/Lading.jsx
+++ client/src/components/Pages/Landing/Lading.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 import "./landing.scss";
+import landing from "../../../assets/images/landing2.gif";
 
 const Landing = () => {
   return (
     <div className="content-wrapper main">
       <div className="container">
-        <h1>Foods</h1>
-        <Link to="/home">
-          <button className="btn">Go</button>
-        </Link>
+        <div className="card-landing">
+          <img src={landing} alt="landing" height="370px" weight="400px" />
+          <h1>Foods</h1>
+          <Link to="/home">
+            <button className="btn">Go</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
